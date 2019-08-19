957c554792e0c97d8ca15d5ecd68d76bf5503ecd
yuanzhijian <jswxyzj@qq.com>
2019-08-19 23:09:32 +0800

    图表显示

--- app/src/main/java/com/drt/moisture/measure/MeasureActivity.java
+++ app/src/main/java/com/drt/moisture/measure/MeasureActivity.java
@@ -1,14 +1,25 @@
 package com.drt.moisture.measure;
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import com.drt.moisture.CustomActionBarActivity;
 import com.drt.moisture.R;
 import com.drt.moisture.data.MeasureValue;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -22,6 +33,9 @@ public class MeasureActivity extends CustomActionBarActivity<MeasurePresenter> i
 
     @BindView(R.id.title_rightImage)
     ImageButton btnBluetooth;
+
+    @BindView(R.id.chart)
+    LineChart chart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,8 +55,36 @@ public class MeasureActivity extends CustomActionBarActivity<MeasurePresenter> i
         mPresenter = new MeasurePresenter();
         mPresenter.attachView(this);
 
+        Point outSize = new Point();
+        this.getWindowManager().getDefaultDisplay().getSize(outSize);
+        chart.setMinimumHeight((outSize.x - getResources().getDimensionPixelSize(R.dimen.padding_default) * 2) / 2);
+
+        // apply styling
+        // holder.chart.setValueTypeface(mTf);
+        chart.getDescription().setEnabled(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(5, false);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setLabelCount(5, false);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
 
 
+        // set data
+        chart.setData(generateDataLine());
+
+        // do not forget to refresh the chart
+        // holder.chart.invalidate();
+        chart.animateX(750);
     }
 
     @Override
@@ -78,5 +120,47 @@ public class MeasureActivity extends CustomActionBarActivity<MeasurePresenter> i
     @OnClick(R.id.title_rightImage)
     public void actionBluetooth() {
 
+    }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return Line data
+     */
+    private LineData generateDataLine() {
+
+        ArrayList<Entry> values1 = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            values1.add(new Entry(i, (int) (Math.random() * 65) + 40));
+        }
+
+        LineDataSet d1 = new LineDataSet(values1, "温度");
+        d1.setLineWidth(2.5f);
+        d1.setCircleRadius(4.5f);
+        d1.setHighLightColor(getResources().getColor(R.color.colorAccent, getTheme()));
+        d1.setColor(getResources().getColor(R.color.colorAccent, getTheme()));
+        d1.setCircleColor(getResources().getColor(R.color.colorAccent, getTheme()));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> values2 = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            values2.add(new Entry(i, values1.get(i).getY() - 30));
+        }
+
+        LineDataSet d2 = new LineDataSet(values2, "水分活度");
+        d2.setLineWidth(2.5f);
+        d2.setCircleRadius(4.5f);
+        d2.setHighLightColor(getResources().getColor(R.color.colorGreen, getTheme()));
+        d2.setColor(getResources().getColor(R.color.colorGreen, getTheme()));
+        d2.setCircleColor(getResources().getColor(R.color.colorGreen, getTheme()));
+        d2.setDrawValues(false);
+
+        ArrayList<ILineDataSet> sets = new ArrayList<>();
+        sets.add(d1);
+        sets.add(d2);
+
+        return new LineData(sets);
     }
 }
